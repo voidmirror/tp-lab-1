b3557acbd53198047f0801f33f12055d9943d47e
AlNualD <50199279+AlNualD@users.noreply.github.com>
2019-09-19 09:54:18 +0300

    tack1

---
+++ src/main1.cpp
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include "task1.h"
+#include <stdio.h>
+
+int main()
+{
+	printf("ppp\n\n");
+	unsigned long min = 1;
+	unsigned long max = 20;
+	printf("%u\n", findValue(min, max));
+	return 0;
+}
