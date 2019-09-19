e4a549bf294c7b648682d710ef5496787443d491
AlNualD <50199279+AlNualD@users.noreply.github.com>
2019-09-19 09:43:15 +0300

    task2

---
+++ src/main2.cpp
@@ -0,0 +1,17 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//
+//
+//#include "task2.h"
+//#include <stdio.h>
+//
+//int main()
+//{
+//	unsigned long long a, n, b;
+//	printf("enter numbs: ");
+//	scanf("%u %u %u", &a, &n, &b);
+//	if (checkPrime(a)) printf("\n a is prime\n");
+//	else printf("a isnt prime \n");
+//	printf("%u prime\n", nPrime(n));
+//	printf("closest prime %u\n", nextPrime(b));
+//	return 0;
+//}
