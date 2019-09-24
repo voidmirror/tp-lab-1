8f633a87b19c158c550d9e6faa7fa7f70221e543
Andrey Talanin <talanin.aa.nnov@gmail.com>
2019-09-24 22:56:24 +0300

    Implement the functions defined in the Task 3

---
+++ src/task3.cpp
@@ -0,0 +1,12 @@
+#include "task2.h"
+#include "task3.h"
+
+uint64_t sumPrime(uint32_t hBound)
+{
+	uint64_t result = 0;
+	for (uint32_t i = 2; i < hBound; i++)
+		if (checkPrime64(i))
+			result += i;
+
+	return result;
+};
