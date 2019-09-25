1234329eb0affd37858bda618ac458dffd14ba11
Doroshenko Ilya <45979573+iddoroshenko@users.noreply.github.com>
2019-09-25 12:26:34 +0300

    Update task3.cpp

--- src/task3.cpp
+++ src/task3.cpp
@@ -1,14 +1,8 @@
 #include "task2.h"
+#include "task3.h"
 #include <iostream>
 using namespace std;
 
-bool checkPrime(unsigned int value){
-	for (unsigned long long i = 2; i * i <= value; i++)
-		if (value % i == 0)
-			return 0;
-	return 1;
-
-}
 unsigned long long sumPrime(unsigned int hbound){
 	unsigned long long answer = 0;
 	for (unsigned long long i = 2; i < hbound; i++)
