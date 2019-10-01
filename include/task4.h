e155fdd71daad4ef160e41f231151332ea774e55
Olga Orlova <olia_orlova2000@mail.ru>
2019-10-01 21:18:42 +0300

    std::reverse -> reverse

--- include/task4.h
+++ include/task4.h
@@ -2,4 +2,5 @@
 #include <algorithm>
 #include <cstdlib>
 #include <string.h>
+char* reverse(char* x, int len);
 char* sum(char* x, char* y);
--- src/task4.cpp
+++ src/task4.cpp
@@ -1,12 +1,26 @@
 #include "task4.h"
 
-char* sum(char* x, char* y) {
-	int len1 = strlen(x), len2 = strlen(y);
-	int len = std::max(len1, len2);
-	char* res = (char*)malloc(len + 2);
+char* reverse(char* x, int len) {
 
-	std::reverse(x, x+len1); 
-	std::reverse(y, y+len2);
+	char* res = (char*)malloc(len);
+	for (int i = 0; i < len; i++) {
+		res[i] = x[len - i - 1];
+	}
+	res[len] = '\0';
+	return res;
+}
+char* sum(char* x, char* y) {
+	char* res;
+	int len1 = strlen(x), len2 = strlen(y);
+
+	int len = len1;
+	if (len1 < len2)
+		len = len2;
+
+	res = (char*)malloc(len + 2);
+
+	x = reverse(x, len1);
+	y = reverse(y, len2);
 
 	int i = 0;
 	int cur = 0;
@@ -34,16 +48,11 @@ char* sum(char* x, char* y) {
 	}
 	if (cur > 0) {
 		res[len] = cur + '0';
-		std::reverse(res, res + len + 1);
-		res[len + 1] = '\0';
+		res = reverse(res, len + 1);
 	}
 	else {
-		std::reverse(res, res + len);
-		res[len] = '\0';
+		res = reverse(res, len);
 	}
 
-	std::reverse(x, x + len1);
-	std::reverse(y, y + len2);
-	
 	return res;
 }
