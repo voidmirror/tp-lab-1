9af0555f1c39158e6902a0b773326a2440c3b692
Voidmirror <voidmirror@users.noreply.github.com>
2019-12-24 20:24:37 +0400

    Correction task4

--- src/task4.cpp
+++ src/task4.cpp
@@ -1,31 +1,72 @@
 #include <iostream>
 #include <string.h>
+#include <vector>
+#include <algorithm>
 #include "task4.h"
 
 using namespace std;
 
-//int countSym(char* str) {
+int countCifer(char* str) {
+	char sym;
+	int counter = 0;
+	/*do {
+		sym = str[i];
+		i++;
+	} while (sym != '\0');
+	return i - 1;*/
+
+	for (int i = 0; i < strlen(str); i++) {
+		if (str[i] >= '0' && str[i] <= '9') {
+			if (!((str[i + 1] > '9' || str[i + 1] < '0') && str[i] == '0')) {
+				counter++;
+			}
+		}
+	}
+	return counter;
+}
+
+//int countCifer222222(char* str) {
 //	char sym;
-//	int i = 0, counter = 0;
-//	do {
-//		sym = str[i];
-//		i++;
+//	int counter = 0;
+//	/*do {
+//	sym = str[i];
+//	i++;
 //	} while (sym != '\0');
-//	return i - 1;
+//	return i - 1;*/
+//
+//	for (int i = 0; i < strlen(str); i++) {
+//		if (str[i] >= '0' && str[i] <= '9') {
+//			counter++;
+//		}
+//	}
+//	return counter;
 //}
 
-char* reverseChar(char* str) {
-	char *_temp;
+//char* reverseChar(char* str) {
+//	char *_temp;
+//
+//	_temp = (char*)malloc(sizeof(char)*strlen(str) + 1);
+//	for (int i = 0; i < strlen(str); i++) {
+//		_temp[i] = str[strlen(str) - 1 - i];
+//		//cout << "str: " << str[strlen(str) - 1 - i] << endl;
+//		//cout << "temp: " << _temp[i] << endl;
+//	}
+//	_temp[strlen(str)] = '\0';
+//	
+//	return _temp;
+//}
 
-	_temp = (char*)malloc(sizeof(char)*strlen(str) + 1);
-	for (int i = 0; i < strlen(str); i++) {
-		_temp[i] = str[strlen(str) - 1 - i];
-		//cout << "str: " << str[strlen(str) - 1 - i] << endl;
-		//cout << "temp: " << _temp[i] << endl;
+void reverseChar2(char* str) {
+	vector <char> copy;
+	copy.resize(countCifer(str));
+	for (int i = 0; i < countCifer(str); i++) {
+		copy[i] = str[i];
 	}
-	_temp[strlen(str)] = '\0';
+	reverse(copy.begin(), copy.end());
+	for (int i = 0; i < countCifer(str); i++) {
+		str[i] = copy[i];
+	}
 	
-	return _temp;
 }
 
 char* sum(char *x, char *y) {
@@ -48,18 +89,29 @@ char* sum(char *x, char *y) {
 		result[i] = '0';
 	}
 
-	x = reverseChar(x);
-	y = reverseChar(y);
+	/*x = reverseChar2(x);
+	y = reverseChar2(y);*/
+	reverseChar2(x);
+	reverseChar2(y);
 	cout << x << " " << y << " " << amount << endl;			//norm
 
 	int remainder = 0;
-	for (int i = 0; i < amount; i++) {
+	for (int i = 0; i < amount - 1; i++) {
 		if (i < sumCounter) {
 			reserve = result[i] - '0' + x[i] + y[i] - '0';
 			cout << "reserve: " << reserve << endl;
 			if (reserve > '9') {
 				result[i] = reserve/* - ('9' - '0' - 1)*/ - 10;
 				result[i + 1]++;
+				//int _tempReserve = i + 1;
+				//int _tempStrlen = strlen(result);
+				//for (int _tempReserveIndex = i + 1; _tempReserveIndex <= amount; _tempReserveIndex) {
+				//	if (result[_tempReserveIndex] > '9') {
+				//		result[_tempReserveIndex] = result[_tempReserveIndex]/* - ('9' - '0' - 1)*/ - 10;
+				//		result[_tempReserveIndex + 1]++;
+				//		//_tempReserve++;
+				//	}
+				//}
 			}
 			else {
 				result[i] = reserve;
@@ -68,13 +120,23 @@ char* sum(char *x, char *y) {
 		else {
 			if (ifX == 1) {
 				result[i] = result[i] + x[i] - '0';
+				if (result[i] > '9') {
+					result[i] = result[i] - 10;
+					result[i + 1]++;
+				}
 			}
 			else {
 				result[i] = result[i] + y[i] - '0';
+				if (result[i] > '9') {
+					result[i] = result[i] - 10;
+					result[i + 1]++;
+				}
 			}
 		}
 	}
 
-	result = reverseChar(result);
+	//result = reverseChar2(result);
+	reverseChar2(result);
+	result[countCifer(result)] = '\0';
 	return result;
 }
---
+++ src/task4.h
@@ -0,0 +1,10 @@
+#ifndef Task4_H
+#define Task4_H
+
+//int countSym(char* str);
+//char* reverseChar(char* str);
+int countCifer(char* str);
+void reverseChar2(char* str);
+char* sum(char *x, char *y);
+
+#endif
